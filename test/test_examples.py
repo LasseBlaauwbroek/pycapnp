d45a170e57f0f50653cf95aa303d3e25bff498bb
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-03-29 17:28:53 +0200

    Bla

--- test/test_examples.py
+++ test/test_examples.py
@@ -126,6 +126,7 @@ def test_async_calculator_example(cleanup):
 
 @pytest.mark.xfail(
     reason="Some versions of python don't like to share ports, don't worry if this fails"
+
 )
 def test_thread_example(cleanup):
     address = "{}:36433".format(hostname)
