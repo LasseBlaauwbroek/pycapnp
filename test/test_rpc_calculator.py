f51b6ee18f33b2bccf50308dfb2a04654e7e7469
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-10-12 11:18:18 +0200

    Remove test_calculator_gc test

--- test/test_rpc_calculator.py
+++ test/test_rpc_calculator.py
@@ -1,4 +1,3 @@
-import gc
 import os
 import socket
 import sys  # add examples dir to sys.path
@@ -26,25 +25,3 @@ async def test_calculator():
 
     _ = capnp.TwoPartyServer(write, bootstrap=async_calculator_server.CalculatorImpl())
     await async_calculator_client.main(read)
-
-
-async def test_calculator_gc():
-    def new_evaluate_impl(old_evaluate_impl):
-        def call(*args, **kwargs):
-            gc.collect()
-            return old_evaluate_impl(*args, **kwargs)
-
-        return call
-
-    read, write = socket.socketpair()
-    read = await capnp.AsyncIoStream.create_connection(sock=read)
-    write = await capnp.AsyncIoStream.create_connection(sock=write)
-
-    # inject a gc.collect to the beginning of every evaluate_impl call
-    evaluate_impl_orig = async_calculator_server.evaluate_impl
-    async_calculator_server.evaluate_impl = new_evaluate_impl(evaluate_impl_orig)
-
-    _ = capnp.TwoPartyServer(write, bootstrap=async_calculator_server.CalculatorImpl())
-    await async_calculator_client.main(read)
-
-    async_calculator_server.evaluate_impl = evaluate_impl_orig
